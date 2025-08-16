4f9bab23375d08b6d70398db92d066ca2507a279
Abled-Taha <abledtaha@gmail.com>
2025-08-17 04:06:52 +0500

    Added downloads page

---
+++ app/downloads/page.tsx
@@ -0,0 +1,24 @@
+export default function downloadsPage(){
+  return(
+    <div>
+      <h1>Downloads</h1>
+      <h2>This page is not supposed to be public and will be updated soon</h2>
+      <br />
+      <a href="/files/resume/resume.pdf" download>
+        <button>Resume.pdf</button>
+      </a>
+      <a href="/files/resume/CV-Syed_Taha_Rizwan.docx" download>
+        <button>CV-Syed_Taha_Rizwan.docx</button>
+      </a>
+      <a href="/files/resume/Cover-Letter_Syed_Taha_Rizwan.pdf" download>
+        <button>Cover-Letter_Syed_Taha_Rizwan.pdf</button>
+      </a>
+      <a href="/files/resume/Cover-Letter_Syed_Taha_Rizwan.docx" download>
+        <button>Cover-Letter_Syed_Taha_Rizwan.docx</button>
+      </a>
+      <a href="/files/resume/CV-Template.docx" download>
+        <button>CV-Template.docx</button>
+      </a>
+    </div>
+  )
+}
